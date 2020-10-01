24862e9796a60822a17b82e79b26a3aadee80d09
Igor Birukov <kpebetko2@mail.ru>
2020-10-01 06:10:59 +0300

    Nothing is working...

--- src/by/epamtc/birukov/entity/User.java
+++ src/by/epamtc/birukov/entity/User.java
@@ -1,4 +1,40 @@
 package by.epamtc.birukov.entity;
 
 public class User {
+    private String email;
+    private String password;
+    private String username;
+    private int role;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
 }
